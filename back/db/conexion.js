e43579479a1691eaeaebaa2c8c708e01f702e398
Andrea Guadalupe Perez Jimenez <andreaprz258@gmail.com>
2025-12-02 19:50:06 -0600

    Una puta coma

--- back/db/conexion.js
+++ back/db/conexion.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({ //2
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
     waitForConnections: true, //3
     connectionLimit: 10, //4
     queueLimit: 0 //5
